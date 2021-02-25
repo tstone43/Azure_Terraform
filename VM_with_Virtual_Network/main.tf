f7bc4065c7cedc5074131e906b72c37caf575c95
tstone43 <tcstone@gmail.com>
2021-02-25 08:14:11 -0800

    adding vm section

--- VM_with_Virtual_Network/main.tf
+++ VM_with_Virtual_Network/main.tf
@@ -20,3 +20,23 @@ resource "azurerm_subnet" "internal" {
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes = ["10.0.2.0/24"]
 }
+
+resource "azurerm_network_interface" "nic0" {
+    name = "${var.prefix}-nic"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip configuration {
+        name = "nic0"
+        subnet_id = azurerm_subnet.internal.id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
+
+resource "azurerm_windows_virtual_machine" "winvm" {
+    name = "${var.prefix}-vm"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    network_interface_ids = [ azurerm_network_interface.nic0.id ]
+    vm_size = "Standard_B1s"
+}
