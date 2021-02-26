cd50c773bcd1c52f6edd05b0d40f51f4cf8b33c1
tstone43 <tcstone@gmail.com>
2021-02-26 08:23:20 -0800

    adding existing key vault still need password referenced

--- VM_with_Virtual_Network/main.tf
+++ VM_with_Virtual_Network/main.tf
@@ -1,9 +1,36 @@
+# Terraform
+terraform {
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "2.47.0"
+        }
+    }
+}
+
+#Azure provider
+provider "azurerm" {
+    features {}
+}
+
 variable "prefix" {
     default = "terraform-winvm"
 }
 
+variable "existing-rg" {
+    default = "thomcstone-kv"
+}
+
+variable "existing-rg-location" {
+    default ="West US 2"
+}
+
+variable "existing-kv-name" {
+    default = "thomcstone-kv"
+}
+
 resource "azurerm_resource_group" "rg" {
-    name = "{var.prefix}-rg"
+    name = "${var.prefix}-rg"
     location = "West US 2"
 }
 
@@ -26,7 +53,7 @@ resource "azurerm_network_interface" "nic0" {
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
 
-    ip configuration {
+    ip_configuration {
         name = "nic0"
         subnet_id = azurerm_subnet.internal.id
         private_ip_address_allocation = "Dynamic"
@@ -38,5 +65,44 @@ resource "azurerm_windows_virtual_machine" "winvm" {
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     network_interface_ids = [ azurerm_network_interface.nic0.id ]
-    vm_size = "Standard_B1s"
+    size = "Standard_B1s"
+    admin_username = "${data.azurerm_key_vault_secret.kvsecret1.value}"
+    admin_password = "value"
+    
+    os_disk {
+        caching = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+        publisher = "MicrosoftWindowsServer"
+        offer     = "WindowsServer"
+        sku       = "2016-Datacenter"
+        version   = "latest"
+    }
+}
+
+resource "azurerm_resource_group" "rg1" {
+    name = var.existing-rg
+    location = var.existing-rg-location
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+    name = var.existing-kv-name
+    location = azurerm_resource_group.rg1.location
+    resource_group_name = azurerm_resource_group.rg1.name
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    sku_name = "standard"
+}
+
+data "azurerm_key_vault" "kvdata" {
+    name = var.existing-kv-name
+    resource_group_name = azurerm_resource_group.rg1.name
+}
+
+data "azurerm_key_vault_secret" "kvsecret1" {
+    name = "WinVMUser"
+    key_vault_id = data.azurerm_key_vault.kvdata.id
 }
